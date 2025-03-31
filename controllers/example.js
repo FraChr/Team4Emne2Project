b88bc5fbc2c22117d4fa25b5b58595a89b79a187
IkayImonoko <ntolok1990@gmail.com>
2025-03-31 12:31:28 +0200

    Nikita: addButtonHTML,  addButtonsHTML,  Nikita/utulities.js, little changing in example.js, linked model in index.html

--- controllers/example.js
+++ controllers/example.js
@@ -1,4 +1,22 @@
 test();
 function test() {
-    document.getElementById("app").innerHTML = `bob`;
+    let courses = [];
+    let events = [];
+    for (let cours of model.data.courses) {
+        courses.push(cours.name);
+    }
+    for (let event of model.data.events) {
+        events.push(event.name);
+    }
+    document.getElementById("app").innerHTML = /*HTML*/`
+        <div>
+            ${createButtonsHTML(courses)} 
+            ${createButtonHTML('Se alle kurs', 'handleSeAlleKurs()')}
+        </div>
+        
+        <div>
+            ${createButtonsHTML(events)} 
+            ${createButtonHTML('Se alle hendelser', 'handleSeAlleHendelser()')}
+        </div>
+    `;
 }
