6590f34ab4c216299aa9e16b5ad261ba791f690a
Ticster <empynia@gmail.com>
2025-04-03 15:18:38 +0200

    added so checkbox check all will trigger the onchange func on individual checkboxes, flushed studentIds array after adding new event

--- Chris/mainPageChris.js
+++ Chris/mainPageChris.js
@@ -1,15 +1,41 @@
 function mainPageChrisView() {
-
+    console.log('TEST');
+    filterStudentStatus();
     return /*html*/ `
     <div class="buttons">
         <div>
             ${createButtonsHTML('courses')} 
         </div>
 
-        <div >
+        <div>
             ${createButtonsHTML('events')} 
         </div>
     </div>
+
+    <select id="userChoiceStatus" onchange="checkButtonStatus()">
+        <option value="" disabled selected>Legg til hendelse</option>
+        <option value="1">Søkt</option>
+        <option value="2">Godkjent</option>
+        <option value="3">Startet</option>
+        <option value="4">Avbrutt</option>
+        <option value="5">Fullført</option>
+        <option value="addPayment">Betal</option>
+    </select>
+
+    <select id="userChoiceCourse" onchange="checkButtonStatus()">
+        <option value="" disabled selected>Velg kurs</option>
+        <option value="1">Start IT</option>
+        <option value="2">Frontend</option>
+        <option value="3">Fagskolen</option>
+        <option value="4">Get Prepared</option>
+        <option value="5">Get IT</option>
+    </select>
+
+    <button disabled id="statusButton" 
+    onclick="changeStudentStatus(document.getElementById('userChoiceStatus').value, 
+    document.getElementById('userChoiceCourse').value)">
+        Oppdater
+    </button>
 
         <table>
             <tr>
@@ -24,44 +50,29 @@ function mainPageChrisView() {
 
 }
 
-// Makes table of all students;
-// function makeTableRow() {
-//     let rows = '';
-//     for (const student of model.data.students) {
-//         rows += /*html*/ `
-//             <tr>
-//                 <td><input type="checkbox" class="checkbox" onclick="updateCheckAll()"/></td>
-//                 <td>
-//                     <span>${student.name}</span>
-//                 </td>
-//                 <td>
-//                     ${getStudentPayment(student.id)}
-//                 </td>
-//                 <td>
-//                     ${getStudentStatus(student.id)}
-//                 </td>
-//             </tr>
-//        `;
-//     }
-//     return rows;
-// }
-
-
 // Makes table based on filteredStudents array in model;
 function makeTableRow() {
     let rows = '';
-    for (const student of model.data.filteredStudents) {
+    for (const status of model.data.filteredStatus) {
         rows += /*html*/ `
             <tr>
-                <td><input type="checkbox" class="checkbox" onclick="updateCheckAll()"/></td>
                 <td>
-                    <span>${getStudent(student.studentId).name}</span>
+                    <input 
+                    type="checkbox" 
+                    class="checkbox" 
+                    onchange="pushStudentId('${status.studentId}')" 
+                    value="${status.name}" 
+                    onclick="updateCheckAll()"
+                    />
                 </td>
                 <td>
-                    ${getStudentPayment(student.studentId)}
+                    <span>${getStudent(status.studentId).name}</span>
                 </td>
                 <td>
-                    ${getStudentStatus(student.studentId)}
+                    ${getStudentPayment(status.studentId)}
+                </td>
+                <td>
+                    ${getStudentStatus(status.studentId)}
                 </td>
             </tr>
        `;
@@ -70,8 +81,10 @@ function makeTableRow() {
 }
 
 function checkAll(source) {
+    const eventType = 'change'
     document.querySelectorAll('.checkbox').forEach(checkbox => {
         checkbox.checked = source.checked;
+        checkbox.dispatchEvent(new Event(eventType));
     });
 }
 
@@ -80,4 +93,43 @@ function updateCheckAll() {
     const checkAll = document.getElementById('checkAll');
 
     checkAll.checked = Array.from(allCheckBoxes).every(checkBox => checkBox.checked);
+}
+
+function createButtonHTML(name, id, buttonsType, buttonClass) {
+    return /*HTML*/ `
+        <button class='${buttonClass ?? ''}' onclick='handleOnclick(${id}, "${buttonsType}")'>${name}</button>
+    `;
+}
+
+function createButtonsHTML(buttonsType) {
+    let html = '';
+    let selectedButtons;
+    let dataSet;
+    let nameForShowAllButton;
+
+    switch (buttonsType) {
+        case 'courses':
+            nameForShowAllButton = 'Sa alle kurs';
+            selectedButtons = model.inputs.mainPage.selectedCurses;
+            dataSet = model.data.courses;
+            break;
+        case 'events':
+            nameForShowAllButton = 'Sa alle hendelser';
+            selectedButtons = model.inputs.mainPage.selectedEvents;
+            dataSet = model.data.events;
+            break;
+    }
+
+    for (const data of dataSet) {
+        let buttonClass = '';
+        if (selectedButtons.includes(data.id) || selectedButtons[0] === 0) {
+            buttonClass = 'pushedButton';
+        }
+        html += /*HTML*/` ${createButtonHTML(data.name, data.id, buttonsType, buttonClass)}`;
+    }
+
+    let buttonClass = selectedButtons[0] === 0 ? 'pushedButton' : '';
+    html += /*HTML*/` ${createButtonHTML(nameForShowAllButton, 0, buttonsType, buttonClass)}`;
+
+    return html;
 }
--- Chris/mainPageChrisController.js
+++ Chris/mainPageChrisController.js
@@ -1,23 +1,111 @@
+function checkButtonStatus() {
+    let userChoiceStatus = document.getElementById('userChoiceStatus');
+    let userChoiceCourse = document.getElementById('userChoiceCourse');
+    if (userChoiceStatus.value && userChoiceCourse.value) {
+        statusButton.disabled = false;
+    }
+    else {
+        statusButton.disabled = true;
+    }
+}
+
+function changeStudentStatus(newStudentStatus, studentCourse) {
+    let statusID = model.data.studentStatus[model.data.studentStatus.length - 1].id;
+    let todaysDate = getNewDate();
+    statusID++;
+
+    if (newStudentStatus == 'addPayment') {
+        addPayment();
+    }
+    else {
+        for (const chosenStudent of model.inputs.mainPage.studentIds) {
+            model.data.studentStatus.push({ id: statusID, eventId: parseInt(newStudentStatus), courseId: parseInt(studentCourse), studentId: parseInt(chosenStudent), date: todaysDate });
+            statusID++;
+        }
+    }
+    console.log(model.data.studentStatus)
+    updateView();
+    model.inputs.mainPage.studentIds = [];
+}
+
+function addPayment() {
+    //Må finne ut om vi skal ha pop up eller ei
+}
+
+function pushStudentId(studentId) {
+    if (model.inputs.mainPage.studentIds.includes(studentId)) {
+        index = model.inputs.mainPage.studentIds.indexOf(studentId);
+        model.inputs.mainPage.studentIds.splice(index, 1);
+    }
+    else {
+        model.inputs.mainPage.studentIds.push(studentId);
+    }
+    console.log(model.inputs.mainPage.studentIds);
+}
+
+function handleOnclick(id, buttonsType) {
+    let selectedButtons;
+    let selectAllButtonId = 0;
+    let firstButtonId = 1;
+
+    switch (buttonsType) {
+        case 'courses':
+            selectedButtons = model.inputs.mainPage.selectedCurses;
+            break;
+        case 'events':
+            selectedButtons = model.inputs.mainPage.selectedEvents;
+            break;
+    }
+
+    if (id === selectAllButtonId) {
+        if (selectedButtons[0] !== selectAllButtonId) {
+            selectedButtons.splice(1);
+            selectedButtons[0] = selectAllButtonId;
+        }
+        else {
+            selectedButtons[0] = firstButtonId;
+        }
+    }
+
+    else {
+        if (selectedButtons.includes(id)) {
+            let index = selectedButtons.indexOf(id);
+            selectedButtons.splice(index, 1);
+            if (selectedButtons.length < 1) {
+                selectedButtons[0] = firstButtonId;
+            }
+        }
+        else {
+            if (selectedButtons[0] !== selectAllButtonId) {
+                selectedButtons.push(id);
+            }
+        }
+    }
+    filterStudentStatus();
+    updateView();
+}
+
+
 function filterStudentStatus() {
     const filtered = [...filterEvents()];
-    model.data.filteredStudents = removeDuplicateStudent(filtered);
-    updateView();
+    model.data.filteredStatus = removeDuplicateStudent(filtered);
+    // updateView();
 }
 
 function filterEvents() {
     const allEvents = 0;
     const courses = filterCourses();
     const events = model.inputs.mainPage.selectedEvents;
-    if(events.includes(allEvents)) {
+    if (events.includes(allEvents)) {
         return courses;
     }
     return courses.filter(course => events.includes(course.eventId))
 }
 
 function filterCourses() {
-    const allSelected = 0;
+    const allCourses = 0;
     const courses = model.inputs.mainPage.selectedCurses;
-    if(courses.includes(allSelected)) {
+    if (courses.includes(allCourses)) {
         return model.data.studentStatus;
     }
     return model.data.studentStatus.filter(status => courses.includes(status.courseId));
@@ -34,13 +122,4 @@ function removeDuplicateStudent(filtered) {
         }
     }
     return unique;
-}
-
-
-
-// Move to individual student page;
-function studentPage(studentId) {
-    model.inputs.studentPage.studendId = studentId;
-    model.app.currentPage = '';
-    updateView();
 }
