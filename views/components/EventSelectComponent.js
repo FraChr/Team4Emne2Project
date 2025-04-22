92ea51f60b1b272b6b6f6e6bfa04c4e106d55d3a
Bethina S.M <bethina.s.moen@gmail.com>
2025-04-22 10:10:51 +0200

    .

--- views/components/EventSelectComponent.js
+++ views/components/EventSelectComponent.js
@@ -1,9 +1,9 @@
-function makeEventButtonsHtml() {
-    return `
-    ${createEventSelector(true)}
-    ${createCourseSelector()}
-    ${createPaymentInput()}
-    ${createUpdateStatusButton()}
+function makeEventButtonsHtml(){
+    return /*HTML*/`
+        ${createEventSelector(true)}
+        ${createCourseSelector()}
+        ${createPaymentInput()}
+        ${createUpdateStatusButton()}
     `;
 }
 
@@ -38,12 +38,12 @@ function makeEventButtonsHtml() {
 function createEventSelector(payment){
     return /*HTML*/`
         <select id="userChoiceStatus" onchange="eventSelectionInput('inputs.mainPage.userChoiceStatus', this.value)" required>
-            <option value="" disabled selected>Legg til hendelse</option>
-            <option value="1">Søkt</option>
-            <option value="2">Godkjent</option>
-            <option value="3">Startet</option>
-            <option value="4">Avbrutt</option>
-            <option value="5">Fullført</option>
+            <option value="" disabled selected> Legg til hendelse </option>
+            <option value="1"> Søkt </option>
+            <option value="2"> Godkjent </option>
+            <option value="3"> Startet </option>
+            <option value="4"> Avbrutt </option>
+            <option value="5"> Fullført </option>
             ${payment ? `<option value="addPayment">Betal</option>` : ''}
         </select>
     `;
@@ -52,23 +52,21 @@ function createEventSelector(payment){
 function createCourseSelector(){
     return /*HTML */`
             <select id="userChoiceCourse" onchange="eventSelectionInput('inputs.mainPage.userChoiceCourse', this.value)" required>
-            <option value="" disabled selected>Velg kurs</option>
-            <option value="1">Start IT</option>
-            <option value="2">Frontend</option>
-            <option value="3">Fagskolen</option>
-            <option value="4">Get Prepared</option>
-            <option value="5">Get IT</option>
+            <option value="" disabled selected> Velg kurs </option>
+            <option value="1"> Start IT </option>
+            <option value="2"> Frontend </option>
+            <option value="3"> Fagskolen </option>
+            <option value="4"> Get Prepared </option>
+            <option value="5"> Get IT </option>
         </select>
     `;
 }
-
-
 
 function createPaymentInput(){
     return /*HTML*/`
         <input disabled type="number" id="paymentAmountInput" onchange="eventSelectionInput('inputs.payment.amount', this.valueAsNumber)" placeholder="Beløp" >
         <input disabled type="date" onchange="eventSelectionInput('inputs.payment.date', this.value)" id="paymentDateInput"> 
-    `
+    `;
 }
 
 function createUpdateStatusButton(){
