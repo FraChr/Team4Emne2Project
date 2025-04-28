791e67446f57a88f8bfaf3ac957e410d9511c413
IkayImonoko <ntolok1990@gmail.com>
2025-04-28 10:35:32 +0200

    Nikita: refakturering

--- views/components/EventSelectComponent.js
+++ views/components/EventSelectComponent.js
@@ -1,4 +1,4 @@
-function makeEventButtonsHtml(){
+function makeEventButtonsHtml() {
     return /*HTML*/`
     <div>
         ${createEventSelector(true)}
@@ -9,41 +9,47 @@ function makeEventButtonsHtml(){
     `;
 }
 
-function createEventSelector(payment){
-    return /*HTML*/`
+function createEventSelector(payment) {
+    let html = /*HTML*/` 
         <select id="userChoiceStatus" onchange="eventSelectionInput('inputs.mainPage.userChoiceStatus', this.value)" required>
-            <option value="" disabled selected> Legg til hendelse </option>
-            <option value="1"> Søkt </option>
-            <option value="2"> Godkjent </option>
-            <option value="3"> Startet </option>
-            <option value="4"> Avbrutt </option>
-            <option value="5"> Fullført </option>
-            ${payment ? `<option value="addPayment">Betal</option>` : ''}
-        </select>
-    `;
+        <option value="" disabled selected> Legg til hendelse </option>`;
+    let counter = 0;
+    html += model.data.events.map(event => {
+        counter++;
+        return /*HTML*/`
+            <option value="${counter}"> ${event.name} </option>
+            `;
+    }).join('');
+    html += /*HTML*/`${payment ? `<option value="addPayment">Betal</option>` : ''} </select>`;
+
+    return html;
 }
 
-function createCourseSelector(){
-    return /*HTML */`
-            <select id="userChoiceCourse" onchange="eventSelectionInput('inputs.mainPage.userChoiceCourse', this.value)" required>
-            <option value="" disabled selected> Velg kurs </option>
-            <option value="1"> Start IT </option>
-            <option value="2"> Frontend </option>
-            <option value="3"> Fagskolen </option>
-            <option value="4"> Get Prepared </option>
-            <option value="5"> Get IT </option>
-        </select>
+function createCourseSelector() {
+    let html = /*HTML*/` 
+        <select id="userChoiceCourse" onchange="eventSelectionInput('inputs.mainPage.userChoiceCourse', this.value)" required>
+        <option value="" disabled selected> Velg kurs </option>
     `;
+    let counter = 0;
+    html += model.data.courses.map(course => {
+        counter++;
+        return /*HTML*/`
+            <option value="${counter}"> ${course.name} </option>
+            `;
+    }).join('');
+    html += /*HTML*/`</select>`;
+
+    return html;
 }
 
-function createPaymentInput(){
+function createPaymentInput() {
     return /*HTML*/`
         <input disabled type="number" id="paymentAmountInput" onchange="eventSelectionInput('inputs.payment.amount', this.valueAsNumber)" placeholder="Beløp" >
         <input disabled type="date" onchange="eventSelectionInput('inputs.payment.date', this.value)" id="paymentDateInput"> 
     `;
 }
 
-function createUpdateStatusButton(){
+function createUpdateStatusButton() {
     return /*HTML*/`
         <button disabled id="statusButton" 
         onclick="changeStudentStatus()">
